4f0b7e6b3e185195492d6c272b06d9f228df28b6
david-mpeter <143358974+david-mpeter@users.noreply.github.com>
2024-03-06 04:44:36 +0100

    git commit -m "first commit" git branch -M main git remote add origin https://github.com/david-mpeter/python_assignement-week-1.git git push -u origin main

---
+++ User_input.py
@@ -0,0 +1,8 @@
+name = input ("What is you name?:  ")
+age = int(input("How old are you? : "))
+location = input("where do you leave?:")
+height = float(input("How tall are you? : "))
+
+print("Hello"+name)
+print("you are"+str(age)+"years old")
+print("you are"+str(height)+"cm tall")
